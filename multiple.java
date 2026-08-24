5b7e653c9630a62734c054e5132b805bd127dcec
Srinath <srinath9787672574@gmail.com>
2026-08-24 11:15:55 +0530

    Add multiplication table program in multiple.java

---
+++ multiple.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class multiple {
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+  for(int i= 1;i<=m;i++){
+    System.out.println(i+"*"+n+"="+(i*n));
+  }
+   }
+    
+}
